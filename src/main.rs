fe21a3591ce29a29ab63650bb3f86e3a37b7ad4e
Alex Morozov <scanhex@gmail.com>
2023-12-12 23:11:57 -0500

    Add nevermore

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,68 @@
-//! This example demonstrates the built-in 3d shapes in Bevy.
-//! The scene includes a patterned texture and a rotation for visualizing the normals and UVs.
-
-use std::f32::consts::PI;
-
-use bevy::{
-    prelude::*,
-    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
-};
+use bevy::{prelude::*, utils::Duration};
+use bevy_stl::StlPlugin;
+use core::f32::consts::PI;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .insert_resource(Msaa::Sample4)
+        .add_plugins(DefaultPlugins)
+        .add_plugins(StlPlugin)
+        .insert_resource(SpinTimer(Timer::from_seconds(1.0 / 60.0, TimerMode::Repeating)))
         .add_systems(Startup, setup)
-        .add_systems(Update, rotate)
+        .add_systems(Update, spin_disc)
         .run();
 }
 
-/// A marker component for our shapes so we can query them separately from the ground plane
 #[derive(Component)]
-struct Shape;
+struct Disc {
+    angle: f32,
+}
 
-const X_EXTENT: f32 = 14.5;
+#[derive(Resource)]
+struct SpinTimer(Timer);
 
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut images: ResMut<Assets<Image>>,
+    asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let debug_material = materials.add(StandardMaterial {
-        base_color_texture: Some(images.add(uv_debug_texture())),
-        ..default()
-    });
-
-    let shapes = [
-        meshes.add(shape::Cube::default().into()),
-        meshes.add(shape::Box::default().into()),
-        meshes.add(shape::Capsule::default().into()),
-        meshes.add(shape::Torus::default().into()),
-        meshes.add(shape::Cylinder::default().into()),
-        meshes.add(shape::Icosphere::default().try_into().unwrap()),
-        meshes.add(shape::UVSphere::default().into()),
-    ];
-
-    let num_shapes = shapes.len();
-
-    for (i, shape) in shapes.into_iter().enumerate() {
-        commands.spawn((
-            PbrBundle {
-                mesh: shape,
-                material: debug_material.clone(),
-                transform: Transform::from_xyz(
-                    -X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                    2.0,
-                    0.0,
-                )
-                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
-                ..default()
-            },
-            Shape,
-        ));
-    }
-
+    commands.spawn((
+        PbrBundle {
+            mesh: asset_server.load("nevermore.stl"),
+            material: materials.add(Color::rgb(0.9, 0.4, 0.3).into()),
+            transform: Transform::from_rotation(Quat::from_rotation_z(0.0)),
+            ..Default::default()
+        },
+        Disc { angle: 0.0 }
+    ));
     commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(30.0, 0.0, 20.0),
         point_light: PointLight {
-            intensity: 9000.0,
-            range: 100.,
-            shadows_enabled: true,
-            ..default()
+            range: 40.0,
+            ..Default::default()
         },
-        transform: Transform::from_xyz(8.0, 16.0, 8.0),
-        ..default()
+        ..Default::default()
     });
-
-    // ground plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(50.0).into()),
-        material: materials.add(Color::SILVER.into()),
-        ..default()
-    });
-
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 6., 12.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
-        ..default()
+        transform: Transform::from_translation(Vec3::new(0.0, -10.0, 10.0))
+            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+        ..Default::default()
     });
 }
 
-fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
-    for mut transform in &mut query {
-        transform.rotate_y(time.delta_seconds() / 2.);
+fn spin_disc(
+    time: Res<Time>,
+    mut timer: ResMut<SpinTimer>,
+    mut query: Query<(&mut Disc, &mut Transform)>,
+) {
+    if timer
+        .0
+        .tick(Duration::from_secs_f32(time.delta_seconds()))
+        .just_finished()
+    {
+        for (mut disc, mut transform) in query.iter_mut() {
+            disc.angle = disc.angle + 0.3 * PI / 180.0;
+            *transform = Transform::from_rotation(Quat::from_rotation_z(disc.angle));
+        }
     }
-}
-
-/// Creates a colorful test pattern
-fn uv_debug_texture() -> Image {
-    const TEXTURE_SIZE: usize = 8;
-
-    let mut palette: [u8; 32] = [
-        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255, 102, 255,
-        198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
-    ];
-
-    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
-    for y in 0..TEXTURE_SIZE {
-        let offset = TEXTURE_SIZE * y * 4;
-        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
-        palette.rotate_right(4);
-    }
-
-    Image::new_fill(
-        Extent3d {
-            width: TEXTURE_SIZE as u32,
-            height: TEXTURE_SIZE as u32,
-            depth_or_array_layers: 1,
-        },
-        TextureDimension::D2,
-        &texture_data,
-        TextureFormat::Rgba8UnormSrgb,
-    )
 }
